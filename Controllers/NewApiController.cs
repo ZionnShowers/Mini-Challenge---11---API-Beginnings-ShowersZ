50a3e5d6d3aa4776fa1db8f92ee6e4b2706d05e2
ZionnShowers <zionn.d.showers@gmail.com>
2026-09-21 11:16:03 -0700

    Moved header to readme

--- Controllers/NewApiController.cs
+++ Controllers/NewApiController.cs
@@ -1,9 +1,3 @@
-//Zionn Showers
-//9-21-2026
-//Mini Challenge # 11 - API Beginnings
-//I basically looked back at the original code to see how I could input the same code but into an API format. I made the user input have the ability to enter their name, add two numbers together, and say the time they wake up.
-//Peer Review:
-//Review:
 using Microsoft.AspNetCore.Mvc;
 
 namespace Mini_Challenge___11___API_Beginnings.Controllers
